97aeb4d0327c2a6640fc669a273535945d6bcf41
yxzhin <yxzhin@gmail.com>
2026-03-28 14:46:33 +0100

    feat: add bootsrap; implement basic card w/ reactive counter && increment button

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,26 @@
-<script setup></script>
+<script setup>
+import { reactive, ref } from "vue"
+const title = ref("vue learn")
+const count = ref(0)
+function increment() {
+  count.value++
+}
+</script>
 
-<template></template>
+<template>
+  <div class="container-fluid">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">{{ title }}</h5>
+        <p class="card-text">count is: {{ count }}</p>
+        <a class="btn btn-primary" @click="increment">increment</a>
+      </div>
+    </div>
+  </div>
+</template>
 
-<style scoped></style>
+<style scoped>
+.card {
+  width: 18rem;
+}
+</style>

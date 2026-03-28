8e0a10facd1b545b71209b5458d766265774a31f
yxzhin <yxzhin@gmail.com>
2026-03-28 14:49:48 +0100

    feat: move counter card to separate component; add editorconfig

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,9 @@
 <script setup>
-import { reactive, ref } from "vue"
-const title = ref("vue learn")
-const count = ref(0)
-function increment() {
-  count.value++
-}
+import CounterCard from "./components/CounterCard.vue"
 </script>
 
 <template>
-  <div class="container-fluid">
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title">{{ title }}</h5>
-        <p class="card-text">count is: {{ count }}</p>
-        <a class="btn btn-primary" @click="increment">increment</a>
-      </div>
-    </div>
-  </div>
+  <CounterCard />
 </template>
 
-<style scoped>
-.card {
-  width: 18rem;
-}
-</style>
+<style scoped></style>
---
+++ src/components/CounterCard.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref } from "vue"
+const title = ref("vue learn")
+const count = ref(0)
+function increment() {
+  count.value++
+}
+</script>
+
+<template>
+  <div class="container-fluid">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">{{ title }}</h5>
+        <p class="card-text">count is: {{ count }}</p>
+        <a class="btn btn-primary" @click="increment">increment</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.card {
+  width: 18rem;
+}
+</style>
